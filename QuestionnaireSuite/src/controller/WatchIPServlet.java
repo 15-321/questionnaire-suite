7ddb486b5055ab719add0140e72ae6d9868fa524
Cedrus <Cedrus@DESKTOP-5I1JJF0>
2016-11-09 17:13:01 +0800

    change

--- QuestionnaireSuite/src/controller/WatchIPServlet.java
+++ QuestionnaireSuite/src/controller/WatchIPServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/WatchIPServlet")
 public class WatchIPServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 2L;
 
     public WatchIPServlet() {
         super();
